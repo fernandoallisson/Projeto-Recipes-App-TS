ba71ef999c8967376d6689ec71383d4ba6bb9bf6
ErickLaporta <erickyanlaportadasilvacontas@gmail.com>
2023-08-21 18:39:10 -0300

    Criando página de Login.

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,1 +1,39 @@
-// Página do Login
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    navigate('/meals');
+    localStorage.setItem('user', JSON.stringify({ email }));
+  };
+
+  return (
+    <label>
+      Email:
+      <input
+        type="email"
+        data-testid="email-input"
+        onChange={ (e) => setEmail(e.target.value) }
+      />
+      Senha:
+      <input
+        type="password"
+        data-testid="password-input"
+        onChange={ (e) => setPassword(e.target.value) }
+      />
+      <button
+        type="submit"
+        data-testid="login-submit-btn"
+        disabled={ !(regexEmail.test(email) && password.length > 6) }
+        onClick={ () => handleSubmit() }
+      >
+        Enter
+      </button>
+    </label>
+  );
+}
---
+++ src/Pages/Meals.tsx
@@ -0,0 +1,7 @@
+export function Meals() {
+  return (
+    <div>
+      <h1>Receitas</h1>
+    </div>
+  );
+}

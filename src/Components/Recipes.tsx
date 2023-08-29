378c0546b3114107586dcb22f50211c68b244d9d
ErickLaporta <erickyanlaportadasilvacontas@gmail.com>
2023-08-28 23:06:04 -0300

    feito requisito 19

--- src/Components/Recipes.tsx
+++ src/Components/Recipes.tsx
@@ -1,72 +1,46 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { RecipesContext } from '../Context/index';
-import { getSearchDrinksByName, getSearchMealsByName } from '../Services/index';
+import { getSearchDrinksByName, getSearchMealsByName } from '../Services';
 
 export function Cards() {
-  const [productsDefault, setProductsDefault] = useState<{ meals: []; }>({ meals: [] }); // products = [meals, drinks
-  const { productsInfo, setHandleProductsInfo } = useContext(RecipesContext);
-  const { meals, drinks } = productsInfo;
+  const [productsToShow, setProductsToShow] = useState<string[]>([]);
   const location = useLocation();
 
-  const combinedProducts = [...(meals || []), ...(drinks || [])];
-  const productsToShow = combinedProducts.slice(0, 12);
+  useEffect(() => {
+    async function fetchProducts() {
+      const responseDrinks = await getSearchDrinksByName('');
+      const dataDrinks = await responseDrinks.drinks;
 
-  const getDefaultProducts = async () => {
-    const mealsProducts = await getSearchMealsByName('');
-    const drinksProducts = await getSearchDrinksByName('');
+      const responseMeals = await getSearchMealsByName('');
+      const dataMeals = await responseMeals.meals;
 
-    if (location.pathname === '/meals') {
-      setHandleProductsInfo(mealsProducts.meals.slice(0, 12));
+      const drinks = dataDrinks;
+      const meals = dataMeals;
+
+      if (location.pathname === '/drinks') setProductsToShow(drinks.slice(0, 12));
+      if (location.pathname === '/meals') setProductsToShow(meals.slice(0, 12));
     }
 
-    if (location.pathname === '/drinks') {
-      setHandleProductsInfo(drinksProducts.drinks.slice(0, 12));
-    }
-  };
-
-  getDefaultProducts();
-  console.log(productsInfo);
+    fetchProducts();
+  }, [location.pathname]);
 
   return (
     <div>
-      {productsToShow.length !== 0 ? (productsToShow.map(
-        (product: any, index: number) => (
-          <div
-            key={ product.idMeal || product.idDrink }
-            data-testid={ `${index}-recipe-card` }
-          >
-            <p data-testid={ `${index}-card-name` }>
-              {product.strMeal
-           || product.strDrink}
-
-            </p>
-            <img
-              src={ product.strMealThumb || product.strDrinkThumb }
-              alt={ product.strMeal || product.strDrink }
-              data-testid={ `${index}-card-img` }
-            />
-          </div>
-        ),
-      )) : (
-        productsInfo.map((product: any, index: number) => (
-          <div
-            key={ product.idMeal || product.idDrink }
-            data-testid={ `${index}-recipe-card` }
-          >
-            <p data-testid={ `${index}-card-name` }>
-              {product.strMeal
-           || product.strDrink}
-
-            </p>
-            <img
-              src={ product.strMealThumb || product.strDrinkThumb }
-              alt={ product.strMeal || product.strDrink }
-              data-testid={ `${index}-card-img` }
-            />
-          </div>
-        ))
-      )}
+      {productsToShow.map((product: any, index: number) => (
+        <div
+          key={ product.idDrink || product.idMeal }
+          data-testid={ `${index}-recipe-card` }
+        >
+          <p data-testid={ `${index}-card-name` }>
+            {product.strDrink || product.strMeal}
+          </p>
+          <img
+            src={ product.strDrinkThumb || product.strMealThumb }
+            alt={ product.strDrink || product.strMeal }
+            data-testid={ `${index}-card-img` }
+          />
+        </div>
+      ))}
     </div>
   );
 }

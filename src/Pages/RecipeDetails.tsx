1e35a59171888ab695c97154e650463abac1743b
fernandoallisson <fernando.alisson@ufpe.br>
2023-08-30 16:42:39 -0300

    Implementação de botão de Start Recipe

--- src/Pages/RecipeDetails.tsx
+++ src/Pages/RecipeDetails.tsx
@@ -101,121 +101,128 @@ export function RecipeDetails() {
 
   return (
     <div>
-      {recipe.meals && (
-        <div>
-          { recipe.meals.map((element: Meal) => (
-            <div key={ element.idMeal }>
-              <h1 data-testid="recipe-title">{ element.strMeal }</h1>
-              <h3 data-testid="recipe-category">{ element.strCategory }</h3>
-              <img
-                src={ element.strMealThumb }
-                alt={ element.strMeal }
-                data-testid="recipe-photo"
-                style={ { width: '300px', height: '200px' } }
-              />
-              <h3>Ingredients</h3>
-              <ul>
-                {
-                  ingredients.map((ingredient, index) => (
-                    <li
-                      key={ ingredient }
-                      data-testid={ `${index}-ingredient-name-and-measure` }
-                    >
-                      { ingredient }
-                      {' '}
-                      -
-                      {' '}
-                      { measure[index] }
-                    </li>
-                  ))
-                }
-              </ul>
-              <h3>Instructions</h3>
-              <p data-testid="instructions">{ element.strInstructions }</p>
-              <iframe
-                data-testid="video"
-                title={ element.strMeal }
-                src={ `https://www.youtube.com/embed/${element.strYoutube.split('v=')[1]}` }
-                width="320"
-                height="240"
-                allowFullScreen
-              />
-            </div>
-          ))}
-        </div>
-      )}
-      {recipe.drinks && (
-        <div>
-          { recipe.drinks.map((element: Drink) => (
-            <div key={ element.idDrink }>
-              <h1 data-testid="recipe-title">{ element.strDrink }</h1>
-              {/* <h3 data-testid="recipe-category">{ element.strCategory }</h3> */}
-              <h3 data-testid="recipe-category">{ element.strAlcoholic }</h3>
-              <img
-                data-testid="recipe-photo"
-                src={ element.strDrinkThumb }
-                alt={ element.strDrink }
-                style={ { width: '10px', height: '10px' } }
-              />
-              <h3>Ingredients</h3>
-              <ul>
-                {
-                  ingredients.map((ingredient, index) => (
-                    <li
-                      key={ ingredient }
-                      data-testid={ `${index}-ingredient-name-and-measure` }
-                    >
-                      { ingredient }
-                      {' '}
-                      -
-                      {' '}
-                      { measure[index] }
-                    </li>
-                  ))
-                }
-              </ul>
-              <h3>Instructions</h3>
-              <p data-testid="instructions">{ element.strInstructions }</p>
-            </div>
-          ))}
-        </div>
-      )}
-      {recomendations.drinks && (
-        <div className="container">
-          { recomendations.drinks.slice(0, 6).map((element: Drink, index: number) => (
-            <div key={ element.idDrink } data-testid={ `${index}-recommendation-card` }>
-              <h1
-                data-testid={ `${index}-recommendation-title` }
-              >
-                { element.strDrink }
-              </h1>
-              <h3 data-testid="recipe-category">{ element.strAlcoholic }</h3>
-              <img
-                data-testid="recipe-photo"
-                src={ element.strDrinkThumb }
-                alt={ element.strDrink }
-                style={ { width: '300px', height: '200px' } }
-              />
-            </div>
-          ))}
-        </div>
-      )}
-      {recomendations.meals && (
-        <div className="container">
-          { recomendations.meals.slice(0, 6).map((element: Meal, index: number) => (
-            <div key={ element.idMeal } data-testid={ `${index}-recommendation-card` }>
-              <h1 data-testid={ `${index}-recommendation-title` }>{ element.strMeal }</h1>
-              <h3 data-testid="recipe-category">{ element.strCategory }</h3>
-              <img
-                data-testid="recipe-photo"
-                src={ element.strMealThumb }
-                alt={ element.strMeal }
-                style={ { width: '350px', height: '350px' } }
-              />
-            </div>
-          ))}
-        </div>
-      )}
+      <div>
+        {recipe.meals && (
+          <div>
+            { recipe.meals.map((element: Meal) => (
+              <div key={ element.idMeal }>
+                <h1 data-testid="recipe-title">{ element.strMeal }</h1>
+                <h3 data-testid="recipe-category">{ element.strCategory }</h3>
+                <img
+                  src={ element.strMealThumb }
+                  alt={ element.strMeal }
+                  data-testid="recipe-photo"
+                  style={ { width: '300px', height: '200px' } }
+                />
+                <h3>Ingredients</h3>
+                <ul>
+                  {
+                    ingredients.map((ingredient, index) => (
+                      <li
+                        key={ ingredient }
+                        data-testid={ `${index}-ingredient-name-and-measure` }
+                      >
+                        { ingredient }
+                        {' '}
+                        -
+                        {' '}
+                        { measure[index] }
+                      </li>
+                    ))
+                  }
+                </ul>
+                <h3>Instructions</h3>
+                <p data-testid="instructions">{ element.strInstructions }</p>
+                <iframe
+                  data-testid="video"
+                  title={ element.strMeal }
+                  src={ `https://www.youtube.com/embed/${element.strYoutube.split('v=')[1]}` }
+                  width="320"
+                  height="240"
+                  allowFullScreen
+                />
+              </div>
+            ))}
+          </div>
+        )}
+        {recipe.drinks && (
+          <div>
+            { recipe.drinks.map((element: Drink) => (
+              <div key={ element.idDrink }>
+                <h1 data-testid="recipe-title">{ element.strDrink }</h1>
+                {/* <h3 data-testid="recipe-category">{ element.strCategory }</h3> */}
+                <h3 data-testid="recipe-category">{ element.strAlcoholic }</h3>
+                <img
+                  data-testid="recipe-photo"
+                  src={ element.strDrinkThumb }
+                  alt={ element.strDrink }
+                  style={ { width: '10px', height: '10px' } }
+                />
+                <h3>Ingredients</h3>
+                <ul>
+                  {
+                    ingredients.map((ingredient, index) => (
+                      <li
+                        key={ ingredient }
+                        data-testid={ `${index}-ingredient-name-and-measure` }
+                      >
+                        { ingredient }
+                        {' '}
+                        -
+                        {' '}
+                        { measure[index] }
+                      </li>
+                    ))
+                  }
+                </ul>
+                <h3>Instructions</h3>
+                <p data-testid="instructions">{ element.strInstructions }</p>
+              </div>
+            ))}
+          </div>
+        )}
+        {recomendations.drinks && (
+          <div className="container">
+            { recomendations.drinks.slice(0, 6).map((element: Drink, index: number) => (
+              <div key={ element.idDrink } data-testid={ `${index}-recommendation-card` }>
+                <h1
+                  data-testid={ `${index}-recommendation-title` }
+                >
+                  { element.strDrink }
+                </h1>
+                <h3 data-testid="recipe-category">{ element.strAlcoholic }</h3>
+                <img
+                  data-testid="recipe-photo"
+                  src={ element.strDrinkThumb }
+                  alt={ element.strDrink }
+                  style={ { width: '300px', height: '200px' } }
+                />
+              </div>
+            ))}
+          </div>
+        )}
+        {recomendations.meals && (
+          <div className="container">
+            { recomendations.meals.slice(0, 6).map((element: Meal, index: number) => (
+              <div key={ element.idMeal } data-testid={ `${index}-recommendation-card` }>
+                <h1
+                  data-testid={ `${index}-recommendation-title` }
+                >
+                  { element.strMeal }
+                </h1>
+                <h3 data-testid="recipe-category">{ element.strCategory }</h3>
+                <img
+                  data-testid="recipe-photo"
+                  src={ element.strMealThumb }
+                  alt={ element.strMeal }
+                  style={ { width: '300px', height: '200px' } }
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <button className="startRecipe" data-testid="start-recipe-btn">Start Recipe</button>
     </div>
   );
 }

fd0e23724d27d148830c94d8a54cc037de96b885
fernandoallisson <fernando.alisson@ufpe.br>
2023-09-01 19:48:47 -0300

    Reiniciando os testes

--- src/tests/RecipeDetails.test.tsx
+++ src/tests/RecipeDetails.test.tsx
@@ -6,6 +6,7 @@ import { mockFetch } from './mock/index';
 import App from '../App';
 
 describe('testando a RecipeDetails', () => {
+  const favoriteBtn = 'favorite-btn';
   mockFetch();
   test('Verifica se na rota [/meals/52977] existe o nome [Corba]', async () => {
     render(
@@ -49,4 +50,53 @@ describe('testando a RecipeDetails', () => {
     const optionalAlcohol = await screen.findByText('Optional alcohol');
     expect(optionalAlcohol).toBeInTheDocument();
   });
+  test('Verfica se Há um botão com uma imagem de like', async () => {
+    render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const inputEmail = await screen.findByTestId('email-input');
+    const inputPassword = await screen.findByTestId('password-input');
+    const btnLogin = await screen.findByTestId('login-submit-btn');
+    const chickenHandi = await screen.findByText('Chicken Handi');
+
+    fireEvent.change(inputEmail, { target: { value: 'email@email.com' } });
+    fireEvent.change(inputPassword, { target: { value: '1234567' } });
+    fireEvent.click(btnLogin);
+    fireEvent.click(chickenHandi);
+    const like = await screen.findByTestId(favoriteBtn);
+    expect(like).toBeInTheDocument();
+
+    fireEvent.click(like);
+
+    expect(like).toBeInTheDocument();
+
+    fireEvent.click(like);
+
+    expect(like).toBeInTheDocument();
+  });
+  // test.only('Verifica se o botão de like funciona', async () => {
+  //   render(
+  //     <MemoryRouter initialEntries={ ['/meals/52977'] }>
+  //       <App />
+  //     </MemoryRouter>,
+  //   );
+  //   const like = await screen.findByTestId(favoriteBtn);
+  //   expect(like).toBeInTheDocument();
+
+  //   fireEvent.click(like);
+  //   console.log(localStorage.getItem('favoriteRecipes'));
+  //   const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes') || '[]');
+  //   expect(favoriteRecipes).toBe([{
+  //     id: '52977',
+  //     type: 'meal',
+  //     nationality: 'Turkish',
+  //     category: 'Side',
+  //     alcoholicOrNot: '',
+  //     name: 'Corba',
+  //     image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
+  //   }]);
+  // });
 });
